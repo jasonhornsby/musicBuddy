f514b460d99198cdffcd54353f51dfc63d643a7e
Jason Hornsby <1996jasonhornsby@gmail.com>
2025-12-02 18:05:47 +1100

    Cleaned up sidebar

--- web/src/lib/components/analyser/analyser.svelte
+++ web/src/lib/components/analyser/analyser.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar';
-	import { ChevronDown } from 'lucide-svelte';
-	import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from '../ui/dropdown-menu';
-	import DropdownMenuTrigger from '../ui/dropdown-menu/dropdown-menu-trigger.svelte';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import * as Item from '$lib/components/ui/item';
+	import { ChevronDown, RotateCcw } from 'lucide-svelte';
 
 	type View = {
 		id: string;
 		name: string;
+		description: string;
 	};
 
 	const views: View[] = [
-		{ id: 'general', name: 'General Information' },
-		{ id: 'waveform', name: 'Waveform' },
-		{ id: 'spectral-flux', name: 'Spectral Flux' },
-		{ id: 'spectrogram', name: 'Spectrogram' }
+		{ id: 'general', name: 'General Information', description: 'Overview of file metadata, duration, sample rate, and channels' },
+		{ id: 'waveform', name: 'Waveform', description: 'Visual representation of amplitude over time' },
+		{ id: 'spectral-flux', name: 'Spectral Flux', description: 'Measure of how quickly the spectrum changes, useful for onset detection' },
+		{ id: 'spectrogram', name: 'Spectrogram', description: 'Frequency content visualization across time' }
 	];
 
 	let selectedView = $state<View>(views[0]);
@@ -27,26 +28,38 @@
 		<Sidebar.Header>
 			<Sidebar.Menu>
 				<Sidebar.MenuItem>
-					<DropdownMenu>
-						<DropdownMenuTrigger>
+					<DropdownMenu.Root>
+						<DropdownMenu.Trigger>
 							{#snippet child({ props })}
 								<Sidebar.MenuButton {...props}>
 									{selectedView.name}
 									<ChevronDown class="ms-auto" />
 								</Sidebar.MenuButton>
 							{/snippet}
-						</DropdownMenuTrigger>
-						<DropdownMenuContent>
+						</DropdownMenu.Trigger>
+						<DropdownMenu.Content class="w-80">
 							{#each views as view (view.id)}
-								<DropdownMenuItem onclick={() => (selectedView = view)}>
-									{view.name}
-								</DropdownMenuItem>
+								<DropdownMenu.Item class="p-0" onclick={() => (selectedView = view)}>
+									<Item.Root size="sm" class="w-full p-2">
+										<Item.Content class="gap-0.5">
+											<Item.Title>{view.name}</Item.Title>
+											<Item.Description>{view.description}</Item.Description>
+										</Item.Content>
+									</Item.Root>
+								</DropdownMenu.Item>
 							{/each}
-						</DropdownMenuContent>
-					</DropdownMenu>
+						</DropdownMenu.Content>
+					</DropdownMenu.Root>
 				</Sidebar.MenuItem>
 			</Sidebar.Menu>
 		</Sidebar.Header>
 		<Sidebar.Content></Sidebar.Content>
+		<Sidebar.Footer>
+			<Sidebar.Menu>
+				<Sidebar.MenuItem>
+					<Sidebar.MenuButton><RotateCcw class="size-4" />Reset file</Sidebar.MenuButton>
+				</Sidebar.MenuItem>
+			</Sidebar.Menu>
+		</Sidebar.Footer>
 	</Sidebar.Root>
 </Sidebar.Provider>
