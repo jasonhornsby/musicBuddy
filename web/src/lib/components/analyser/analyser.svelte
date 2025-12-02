e683f8a00eda980922eda5265ba85a370f110340
Jason Hornsby <1996jasonhornsby@gmail.com>
2025-12-02 17:36:50 +1100

    Basic UI work

--- web/src/lib/components/analyser/analyser.svelte
+++ web/src/lib/components/analyser/analyser.svelte
@@ -1,3 +1,27 @@
-<div class="h-full w-full">
-	<h1>This is the analyser</h1>
-</div>
+<script lang="ts">
+    import * as Sidebar from '$lib/components/ui/sidebar';
+</script>
+
+<Sidebar.Provider>
+	<div class="h-full w-full">
+		<main></main>
+	</div>
+	<Sidebar.Root side="right">
+		<Sidebar.Header>
+			<h1>Analyser</h1>
+		</Sidebar.Header>
+		<Sidebar.Content>
+			<Sidebar.Group>
+				<Sidebar.GroupLabel>Views</Sidebar.GroupLabel>
+				<Sidebar.GroupContent>
+					<Sidebar.Menu>
+						<Sidebar.MenuButton>General Information</Sidebar.MenuButton>
+						<Sidebar.MenuButton>Waveform</Sidebar.MenuButton>
+						<Sidebar.MenuButton>Spectral Flux</Sidebar.MenuButton>
+						<Sidebar.MenuButton>Spectrogram</Sidebar.MenuButton>
+					</Sidebar.Menu>
+				</Sidebar.GroupContent>
+			</Sidebar.Group>
+		</Sidebar.Content>
+	</Sidebar.Root>
+</Sidebar.Provider>
